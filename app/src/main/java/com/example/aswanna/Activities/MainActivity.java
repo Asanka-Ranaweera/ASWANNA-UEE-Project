74108b774ffa4618fcbddf361e1235e2ed4cc7c5
IT21182396 <99359954+IT21182396@users.noreply.github.com>
2023-10-19 20:53:47 +0530

    Login part and Register part done

--- app/src/main/java/com/example/aswanna/Activities/MainActivity.java
+++ app/src/main/java/com/example/aswanna/Activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.aswanna.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.aswanna.R;
 
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/aswanna/Model/User.java
@@ -0,0 +1,17 @@
+package com.example.aswanna.Model;
+
+public class User {
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_USER_TYPE ="usertype";
+    public static final String KEY_EMAIL= "email";
+    public static final String KEY_PASSWORD ="password";
+    public static final String KEY_PHONE_NO ="phoneno";
+    public static final String KEY_CONFIRM_PASSWORD ="confpassword";
+    public static final String KEY_PREFERENCE_NAME ="chatAppPreference";
+    public static final String KEY_IS_SIGNED_IN ="isSignedIn";
+    public static final String KEY_USER_ID ="userId";
+    public static final String KEY_IMAGE ="image";
+    public static final String KEY_LEVEL="level";
+    public static final String KEY_COUNT="count";
+}
